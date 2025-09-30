bf6186b5ef7f3ad22e72405a17b4cd09db5604c6
Daniil <80905799+DaniilGavrin@users.noreply.github.com>
2025-09-30 16:08:08 +0300

    V0.0.1.1

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:lixem_gang/screens/not_found_screen.dart';
 
 import 'screens/home_screen.dart';
 
